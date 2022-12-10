0e41f1d1bc857a3f937583d9b7c7af0782379f98
fairy <fairyplacesss@gmail.com>
2022-12-10 22:52:37 +0500

    init

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,40 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<div class="wrapper">
+  <div class="form">
+    <input type="text" class="form__input">
+    <input type="text" class="form__input">
+    <button>Create post</button>
+  </div>
+  <div class="posts">
+    <div class="post" v-for="post in posts">
+      <div class="post__title">
+        {{post.title}}
+      </div>
+      <div class="post__body">
+        {{post.body}}
+      </div>
+    </div>
+  </div>
+</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data(){
+    return{
+      posts: [
+        {title: "title", body: "body"},
+        {title: "title", body: "body"},
+        {title: "title", body: "body"},
+
+      ]
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
